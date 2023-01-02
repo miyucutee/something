6efcb2d521a9487dc7e4d67cbc8e2acc588d628f
miyu <kroniikbar@gmail.com>
2023-01-02 14:00:17 +0700

    fix: 🐞 bugfix

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 import { Options } from './hooks/hooks';
 declare module 'hooks-beta' {
   function useCounter(initialValue: number, options: Options): { count: number, increment: () => void };
+  function useCounter(initialValue: number): { count: number, increment: () => void };
 }
